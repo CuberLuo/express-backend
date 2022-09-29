fd155644fb02bbefa0ac2f140544e5bc38651301
CuberLuo <1501858187@qq.com>
2022-09-29 11:30:55 +0800

    update

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express')
 const app = express()
+
+// 解决跨域问题
+app.all('*', function (req, res, next) {
+  // 设置允许跨域的域名,*代表允许任意域名跨域
+  res.header('Access-Control-Allow-Origin', '*')
+  // 允许的header类型
+  res.header('Access-Control-Allow-Headers', 'content-type')
+  // 跨域允许的请求方式
+  res.header('Access-Control-Allow-Methods', 'DELETE,PUT,POST,GET,OPTIONS')
+  if (req.method.toLowerCase() == 'options')
+    res.send(200) // 让options 尝试请求快速结束
+  else next()
+})
+
 // 后端请求拦截器
 app.use((request, response, next) => {
   console.log('请求接口:', request.url)
@@ -13,6 +27,17 @@ app.get('/', (request, response) => {
     code: 10000,
     msg: 'Hello World!',
     data: null
+  }
+  response.send(res)
+})
+
+app.get('/test', (request, response) => {
+  const res = {
+    code: 10000,
+    msg: 'Test',
+    data: {
+      token: '12i3j1293j1'
+    }
   }
   response.send(res)
 })
